6a178530111027fa02c87bf00983a07583cc638f
darkwraithcovenant <joseop@hotmail.com>
2023-04-07 18:13:38 -0700

    styling fix for button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+
   }
   
   // Close the dropdown if the user clicks outside of it
